3f4df7b4c73136721502d5decb668bc66a520f61
SDGY <154787650+dlsntos@users.noreply.github.com>
2025-03-30 13:07:33 +0800

    added forms (username and password)

--- public/login.php
+++ public/login.php
@@ -7,7 +7,15 @@
 </head>
 <body>
     <div id="login-container">
-        <div id="forms-container"></div>
+        <div id="forms-container">
+            <img src="" alt="">
+            <h1>Log in</h1>
+            <form action="">
+                <input type="text" name="username" placeholder="Username" required>
+                <input type="password" name="password" placeholder="Password" required>
+                <button type="submit">Login</button>
+            </form>
+        </div>
     </div>
 </body>
 </html>
